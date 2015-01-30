db2443df942a08247e75f75fa050e4c72612630a
Henry Q. Dineen <henry.dineen@gmail.com>
2015-01-30 10:29:03 -0500

    working on master version

--- src/test/java/EmailFormTest.java
+++ src/test/java/EmailFormTest.java
@@ -10,28 +10,35 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 public class EmailFormTest {
 
 	private WebDriver driver;
-
-	@BeforeMethod
-	public void beforeMethod() {
-		driver = new FirefoxDriver();
-	}
-
-	@AfterMethod
-	public void after() {
-		driver.quit();
-	}
+//
+//	@BeforeMethod
+//	public void beforeMethod() {
+//		driver = new FirefoxDriver();
+//	}
+//
+//	@AfterMethod
+//	public void after() {
+//		driver.quit();
+//	}
+//
+//	@Test
+//	public void test() {
+//		driver.get("http://localhost:3000");
+//
+//		EmailForm emailForm = new EmailForm(driver);
+//
+//		emailForm.clickComposeButton();
+//		emailForm.sendEmail("test@example.com", "WebDriver", "Automate everything!");
+//	}
 
 	@Test
 	public void test() {
-		driver.get("http://localhost:3000");
-
-		EmailForm emailForm = new EmailForm(driver);
-
-		emailForm.clickComposeButton();
-		emailForm.sendEmail("test@example.com", "WebDriver", "Automate everything!");
+		assertTrue(false);
 	}
 
 	private class EmailForm {
