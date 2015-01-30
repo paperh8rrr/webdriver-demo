303a9ad643494488c92718ce2d14ab0faa8a0efa
Henry Q. Dineen <henry.dineen@gmail.com>
2015-01-30 09:24:16 -0500

    step-1 changes

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # webdriver-demo-basic
 
 ## step-0
-Basic test using the WebDriver Java API
+Test using RemoteWebDriver and Standalone Selenium Server
 
 
 ```sh
+$ curl -OL http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar
+
+$ java -jar selenium-server-standalone-2.44.0.jar
+
 $ mvn test -Dmaven.test.skip=false
 ```
+
+https://code.google.com/p/selenium/wiki/DesiredCapabilities
+
+http://www.seleniumhq.org/docs/03_webdriver.jsp#running-standalone-selenium-server-for-use-with-remotedrivers
--- src/test/java/EmailFormTest.java
+++ src/test/java/EmailFormTest.java
@@ -1,19 +1,28 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class EmailFormTest {
 
 	private WebDriver driver;
 
 	@BeforeMethod
-	public void beforeMethod() {
-		driver = new FirefoxDriver();
+	public void beforeMethod() throws MalformedURLException {
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		URL remoteAddress = new URL("http://localhost:4444/wd/hub");
+		driver = new RemoteWebDriver(remoteAddress, capabilities);
 	}
 
 	@AfterMethod
