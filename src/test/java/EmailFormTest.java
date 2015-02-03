1740dfa01b31a5a0e6eeac73fbc0c53d4cbcaf7a
Henry Q. Dineen <henry.dineen@gmail.com>
2015-02-03 16:48:30 -0500

    implicit wait example

--- src/test/java/EmailFormTest.java
+++ src/test/java/EmailFormTest.java
@@ -1,11 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class EmailFormTest {
 
@@ -24,20 +24,16 @@ public class EmailFormTest {
 	@Test
 	public void test() {
 		driver.get("http://localhost:3000");
-
-		WebDriverWait wait = new WebDriverWait(driver, 5);
-
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("welcome-screen")));
-
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
 		driver.findElement(By.className("compose-button")).click();
-
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("email-form")));
 
 		driver.findElement(By.name("to")).sendKeys("test@example.com");
 		driver.findElement(By.name("subject")).sendKeys("WebDriver");
 		driver.findElement(By.name("text")).sendKeys("Automate everything!");;
 		driver.findElement(By.className("send-button")).click();
 
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("feedback-view")));
+		driver.findElement(By.className("feedback-view"));
 	}
 }
